1528d8395ec3e31b657400c379205825311dbfe2
Rod MP <ramp.bng@gmail.com>
2025-02-28 12:34:13 -0600

    updated routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import express from 'express';
+import path from 'path';
 import routes from './routes/index.js';
 import { sequelize } from './models/index.js';
 
@@ -11,10 +12,15 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Serves static files in the entire client's dist folder
-app.use(express.static('../client/dist'));
+app.use(express.static(path.join(__dirname, '../../client/dist')));
 
 app.use(express.json());
 app.use(routes);
+
+// Serve the client application for all other routes
+app.get('*', (_req, res) => {
+  res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
+});
 
 sequelize.sync({force: forceDatabaseRefresh}).then(() => {
   app.listen(PORT, () => {
